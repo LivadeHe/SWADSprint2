d66ba8c892a268b8aa96c97910fb105f0193cef6
Jakob Moestl <fhb211129@fh-vie.ac.at>
2023-10-25 12:54:47 +0200

    First repositories for book and author added

--- jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Repository/AuthorRepository.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Repository/AuthorRepository.java
@@ -1,4 +1,14 @@
 package at.fhbfi.pit.jpademo.persistence.Repository;
 
-public interface AuthorRepository {
+import at.fhbfi.pit.jpademo.persistence.Entity.AuthorEntity;
+import at.fhbfi.pit.jpademo.persistence.Entity.HobbyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<HobbyEntity, Long> {
+    List<AuthorEntity> findByName (String name);
+    List<AuthorEntity> findByBook (String title);
+
+
 }
--- jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Repository/BookRepository.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Repository/BookRepository.java
@@ -1,4 +1,13 @@
 package at.fhbfi.pit.jpademo.persistence.Repository;
 
-public interface BookRepository {
+import at.fhbfi.pit.jpademo.persistence.Entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
+    List<BookEntity> findByTitle(String title);
+    List<BookEntity> findByAuthor(String author);
+
 }
