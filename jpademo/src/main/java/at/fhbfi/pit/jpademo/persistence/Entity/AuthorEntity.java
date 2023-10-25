68f293d16f492c52e8b1235db1a1ae126a66fb65
Jakob Moestl <fhb211129@fh-vie.ac.at>
2023-10-25 12:45:32 +0200

    Simple entity for author and book added

--- jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Entity/AuthorEntity.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Entity/AuthorEntity.java
@@ -1,6 +1,32 @@
 package at.fhbfi.pit.jpademo.persistence.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+
 public class AuthorEntity {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    @Column(name = "auth_id")
+    private long id;
+
+    @Column(name = "auth_name")
+    private String name;
+
+    @Column(name = "auth_age")
+    private int age;
+
+
 
 
 
--- jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Entity/BookEntity.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Entity/BookEntity.java
@@ -1,4 +1,25 @@
 package at.fhbfi.pit.jpademo.persistence.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+
 public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "book_title")
+    private String title;
+
+    @Column(name = "book_author")
+    private String author;
 }
