642223b31e5076a75cc364469115c5fdccb50076
Jakob Moestl <fhb211129@fh-vie.ac.at>
2023-10-26 20:30:39 +0200

    Initaliser für ManyToMany Join geadded

--- jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Repository/BookRepository.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/persistence/Repository/BookRepository.java
@@ -3,14 +3,15 @@ package at.fhbfi.pit.jpademo.persistence.Repository;
 import at.fhbfi.pit.jpademo.persistence.Entity.AuthorEntity;
 import at.fhbfi.pit.jpademo.persistence.Entity.BookEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
     List<BookEntity> findByTitle(String title);
 
-    List<BookEntity> findBooksByLinked_authors(AuthorEntity author);
-    //  List<BookEntity> findByLinked_authors(String author);
+    //List<BookEntity> findBooksByLinked_authors(AuthorEntity author);
 
 }
