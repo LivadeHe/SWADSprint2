8f9b9b40db00801dc9a85495a97800540d3d958e
Jakob Moestl <fhb211129@fh-vie.ac.at>
2023-11-13 16:10:20 +0100

    Auther Dto build

--- jpademo/src/main/java/at/fhbfi/pit/jpademo/Service/dto/AuthorDto.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/Service/dto/AuthorDto.java
@@ -1,9 +1,16 @@
 package at.fhbfi.pit.jpademo.Service.dto;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
 public class AuthorDto {
 
     private Long id;
     private String name;
-    private int mail;
+    private String mail;
 
 }
