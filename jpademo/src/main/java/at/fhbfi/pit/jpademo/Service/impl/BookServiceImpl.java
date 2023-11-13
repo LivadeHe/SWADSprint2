e31e4318dbab1c162b98c8cdb2ef5ff39ce22fa1
Jakob Moestl <fhb211129@fh-vie.ac.at>
2023-11-13 16:12:29 +0100

    Book Service Impl built

--- jpademo/src/main/java/at/fhbfi/pit/jpademo/Service/impl/BookServiceImpl.java
+++ jpademo/src/main/java/at/fhbfi/pit/jpademo/Service/impl/BookServiceImpl.java
@@ -1,7 +1,36 @@
 package at.fhbfi.pit.jpademo.Service.impl;
 
+import at.fhbfi.pit.jpademo.Service.BookService;
+import at.fhbfi.pit.jpademo.Service.dto.BookDto;
+import at.fhbfi.pit.jpademo.Service.mapper.BookMapper;
+import at.fhbfi.pit.jpademo.persistence.Repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class BookServiceImpl {
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private BookMapper bookMapper;
+
+    @Override
+    public List<BookDto> getBookList() {
+        return bookMapper.toDto(bookRepository.findAll());
+    }
+
+    @Override
+    public void save(final BookDto book) {
+        bookRepository.save(bookMapper.toEntity(book));
+
+    }
+
+    @Override
+    public BookDto getBook(final long id) {
+        return bookMapper.toDto(bookRepository.findById(id).orElseGet(null));
+    }
 }
